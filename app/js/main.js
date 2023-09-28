d466f113c082a928a386ebfb16efd285dd450aa9
Anna Danilova <agdanilova@mail.ru>
2023-09-28 19:52:05 +0300

    add animation MAIN-MENU

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
+'use strict';
+
 const info = document.querySelector('.info');
 const buttonInfo = document.querySelector('.info-button');
 const infoClose = document.querySelector('.button--info-close');
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelectorAll('.main-menu');
 
 buttonInfo.addEventListener('click', function () {
   info.classList.remove('info--close');
@@ -8,4 +12,20 @@ buttonInfo.addEventListener('click', function () {
 
 infoClose.addEventListener('click', function () {
   info.classList.add('info--close');
+});
+
+hamburger.addEventListener('click', function () {
+  Array.from(menu).forEach(function (menuItem) {
+    menuItem.classList.toggle('main-menu--open');
+  });
+  hamburger.classList.toggle('hamburger--close');
+});
+
+window.addEventListener('click', e => {
+  const target = e.target
+  if (!target.closest('.main-menu') && !target.closest('.hamburger')) {
+    Array.from(menu).forEach(function (menuItem) {
+      menuItem.classList.remove('main-menu--open');
+    })
+  }
 });
